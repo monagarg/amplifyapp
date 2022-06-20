f6cb5fa26f7652783e6e21ea6ce28b98926bf63a
Mona Garg <monagarg@amazon.com>
2022-06-19 17:59:24 -0700

    update text

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello all</h1>
+        <h1>Hello All</h1>
       </header>
     </div>
   );
